ee3f5edcc1422dd56d92aaedc31096e63e8d5a89
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-10-08 13:24:37 +0900

    feat : 프로젝트 생성 및 dependency 추가

---
+++ WeatherTCA/WeatherTCA/WeatherTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTCAApp.swift
+//  WeatherTCA
+//
+//  Created by 조성민 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
